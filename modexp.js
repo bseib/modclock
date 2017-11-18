39e168d5b8630acb4d7649cddfadd004a904f3b2
Broc Seib <broc.seib@gmail.com>
2017-11-18 15:54:19 -0500

    complete working demo

--- modexp.js
+++ modexp.js
@@ -4,13 +4,43 @@
 
     Vue.component('mod-exp', {
       template: '#modexp-tmpl',
-      props: ['yo'],
+      props: ['id', 'kind'],
       data: function() {
-        return {};
+        return {
+          'B': undefined,
+          'e': undefined,
+          'm': undefined,
+          'A': undefined,
+        };
       },
       computed: {
-        yoyo: function() {
-          return "Sandy Duncan";
+        isShowSolver: function() {
+          return "solver" == this.kind;
+        },
+        isShowComment: function() {
+          return "comment" == this.kind;
+        },
+      },
+      methods: {
+        close: function() {
+          this.$emit('close-solver', this.id);
+        },
+        computeA: function() {
+          if ( this.m == 0 ) {
+            this.A = "?";
+          }
+          else if ( 'undefined' == typeof this.B ) {
+            this.A = "?";
+          }
+          else if ( 'undefined' == typeof this.e ) {
+            this.A = "?";
+          }
+          else if ( 'undefined' == typeof this.m ) {
+            this.A = "?";
+          }
+          else {
+            this.A = bigInt(this.B).modPow(this.e, this.m).toJSNumber();
+          }
         }
       },
     });
@@ -18,15 +48,38 @@
     var app = new Vue({
       el: '#mod-exp-app',
       data: {
-        solvers: ["x"],
+        id: 0,
+        solvers: [{id: 0, kind: "solver"}],
       },
       computed: {},
       methods: {
+        closeSolver: function(closeId) {
+//          debugger;
+          var foundIndex = -1;
+          for (var i=0;i<this.solvers.length;i++) {
+            if ( closeId == this.solvers[i].id ) {
+              foundIndex = i;
+              break;
+            }
+          }
+          if ( foundIndex > -1 ) {
+            Vue.delete(this.solvers, foundIndex);
+          }
+        },
         addSolver: function () {
-          this.solvers.unshift("x");
+          this.id++;
+          this.solvers.push({id: this.id, kind: "solver"});
+        },
+        addComment: function () {
+          this.id++;
+          this.solvers.push({id: this.id, kind: "comment"});
         }
       },
     });
+
+    // app.$on('close', function(id) {
+    //   console.log("close " + id);
+    // });
 
   }, false);
 }());
